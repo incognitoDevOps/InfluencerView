748e2076ddbeb003904ebeae8edcfaa4a8825745
incognitoDevOps <in110005620@kisiiuniversity.ac.ke>
2025-02-01 22:51:17 +0300

    updates

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,27 @@
 import Image from "next/image";
+import * as React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
 
 export default function Home() {
   return (
     <div>
-    hello world
+
+    <BarChart
+      series={[
+        { data: [4, 2, 5, 4, 1], stack: 'A', label: 'Series A1' },
+        { data: [2, 8, 1, 3, 1], stack: 'A', label: 'Series A2' },
+        { data: [14, 6, 5, 8, 9], label: 'Series B1' },
+      ]}
+      barLabel={(item: { value: { toString: () => any; }; }, context: { bar: { height: number; }; }) => {
+        if ((item.value ?? 0) > 10) {
+          return 'High';
+        }
+        return context.bar.height < 60 ? null : item.value?.toString();
+      }}
+      width={600}
+      height={350}
+    />
+
     </div>
   );
 }
